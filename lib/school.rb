ab7d936705597b43d15c09d4ba80824c9afa5ef9
Megan Horsfield <meganthadev@gmail.com>
2020-02-23 07:55:34 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,17 +1,27 @@
-class School 
+class School
   attr_reader :name
   attr_accessor :roster
+
   def initialize(name)
     @name = name
     @roster = {}
-  end 
-  
-  def school_name(name)
-    @name = name 
   end
-  def roster(roster)
-    @roster = roster 
+
+  def add_student(name, grade_lvl)
+    if @roster.key?(grade_lvl)
+      @roster[grade_lvl] << name
+    else
+      @roster[grade_lvl] = []
+      roster[grade_lvl] << name
+    end
   end
-end  
 
+  def grade(grade_lvl)
+    @roster[grade_lvl]
+  end
 
+  def sort
+    @roster.each{|k,v| v.sort!}
+  end
+
+end
